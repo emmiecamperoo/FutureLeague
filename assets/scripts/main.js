25e3915b15017df69d6597fd29f4b2e520a28806
Jan Perse <janperse@Jans-MacBook-Pro.local>
2016-03-25 14:12:12 +0100

    Add: mailchimp js in main.js, separated jquery just in case it's needed and cdn is not used

---
+++ assets/scripts/main.js
@@ -0,0 +1,9 @@
+(function($) {
+	window.fnames = new Array();
+	window.ftypes = new Array();
+	fnames[0] = 'EMAIL';
+	ftypes[0] = 'email';
+	fnames[1] = 'ZCODE';
+	ftypes[1] = 'text';
+}(jQuery));
+var $mcj = jQuery.noConflict(true);
