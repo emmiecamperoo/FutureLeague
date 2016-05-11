3fa943e8b23c37c12c23e54c078e02379f9e02af
Jan Perse <janperse@Jans-MacBook-Pro.local>
2016-05-11 12:18:39 +0200

    Remove: dist from gitignore | Fix: package and bower

--- dist/scripts/main.js
+++
@@ -1,2 +0,0 @@
-$(function(e){window.fnames=new Array,window.ftypes=new Array,fnames[0]="EMAIL",ftypes[0]="email",fnames[1]="ZCODE",ftypes[1]="text",fnames[2]="MMERGE2",ftypes[2]="dropdown",e(".btn--submit").click(function(){e("#mc_embed_signup h3, #mc_embed_signup form .row").hide()})}(jQuery));var $mcj=jQuery.noConflict(!0);
-//# sourceMappingURL=main.js.map
